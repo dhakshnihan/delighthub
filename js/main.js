876ebf67715157301c0cd9e6c8030666baaf9e3b
venky <venkataramana.challa95@gmail.com>
2022-12-07 15:44:37 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -152,26 +152,26 @@ $('.wish').on('click', function(){
 //========================================
 //      INCREMENT PRODUCT QUANTITY
 //========================================
-$('.action-plus').on('click', function(){
-    var increamentValue = $(this).closest('.product-action').children('.action-input').get(0).value++
-    var actionMinus = $(this).closest('.product-action').children('.action-minus');
+// $('.action-plus').on('click', function(){
+//     var increamentValue = $(this).closest('.product-action').children('.action-input').get(0).value++
+//     var actionMinus = $(this).closest('.product-action').children('.action-minus');
 
-    if(increamentValue > 0) {
-        actionMinus.removeAttr('disabled');
-    }
-});
+//     if(increamentValue > 0) {
+//         actionMinus.removeAttr('disabled');
+//     }
+// });
 
 
 //========================================
 //      DECREMENT PRODUCT QUANTITY
 //========================================
-$('.action-minus').on('click', function(){
-    var decrementValue = $(this).closest('.product-action').children('.action-input').get(0).value--
+// $('.action-minus').on('click', function(){
+//     var decrementValue = $(this).closest('.product-action').children('.action-input').get(0).value--
 
-    if(decrementValue == 2) {
-        $(this).attr('disabled', 'disabled');
-    }
-});
+//     if(decrementValue == 2) {
+//         $(this).attr('disabled', 'disabled');
+//     }
+// });
 
 
 //========================================
